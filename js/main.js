9ea4186dfc6dfc0c9cfdbae8add596ab1d6a144f
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-08 20:28:31 +0100

    formattazione data

--- js/main.js
+++ js/main.js
@@ -66,34 +66,51 @@ const posts = [
   
 let arrayId = [];
 let likesNumber = 0;
+let year;
+let month;
+let day;
 
 let postList = document.getElementById("container");
+
 // Clonazione di un template html
 for(let i = 0; i < posts.length; i++) {
     const singlePost = posts[i];
-    // console.log(singlePost.created);
-    let year = singlePost.created.indexOf(4,3);
-    // console.log(year);
-
 
     const postContainer = document.getElementById('template-post').content.cloneNode(true);
     postContainer.querySelector('.post__image img').src = singlePost.media;
+
     if( singlePost.author.image ) {
         postContainer.querySelector('.profile-pic').src = singlePost.author.image;
     } else {
         postContainer.querySelector('.profile-pic').remove();
         postContainer.querySelector('.post-meta__icon').innerHTML = onlyCapitalLetters(singlePost.author.name);
     }
+
     postContainer.querySelector('.post-meta__author').innerHTML = singlePost.author.name;
-    postContainer.querySelector('.post-meta__time').innerHTML = singlePost.created;
+
+    //Formattazione data
+    for ( let key in posts){
+        console.log(posts[key].created);
+        let date = posts[key].created;
+        day = date.slice(8,10);
+        console.log(day);
+        month = date.slice(5,7);
+        console.log(month);
+        year = date.slice(0,4);
+        console.log(year);
+    }
+    postContainer.querySelector('.post-meta__time').innerHTML = `${day}/${month}/${year}`;
     
     postContainer.querySelector('.post__text').innerHTML = singlePost.content;
     postContainer.querySelector('img').src = singlePost.media;
+
     let likeButton = postContainer.querySelector('.like-button.js-like-button');
     likeButton.setAttribute('data-postid', singlePost.id);
+    
     let counter = postContainer.querySelector('.js-likes-counter')
     counter.innerHTML = singlePost.likes;
 
+    //click mi piace
     likeButton.addEventListener('click', function(){
         likeButton.classList.toggle('like-button--liked');
         likesNumber += 1;
@@ -103,7 +120,6 @@ for(let i = 0; i < posts.length; i++) {
         if (classExists) {
             counter.innerHTML = singlePost.likes + 1;
             arrayId.push(singlePost.id);
-            console.log(arrayId);
         } else {
             counter.innerHTML = singlePost.likes ;
         }
@@ -115,32 +131,3 @@ for(let i = 0; i < posts.length; i++) {
 }
 
 
-
-// let year;
-// let month;
-// let day;
-
-// for ( let key in posts){
-//     console.log(posts[key].created);
-//     let date = posts[key].created;
-//     for ( let i = 0; i < 4; i++) {
-//         // console.log(date[i]);
-//         year = Number(date[i]);
-//         // year.push(date[i]);
-//         console.log(year);
-//     }
-//     for ( let i = 5; i < 7; i++) {
-//         // console.log(date[i]);
-//         month = date[i];
-//         // console.log(month);
-//     }
-//     for ( let i = 8; i < 11; i++) {
-//         // console.log(date[i]);
-//         day = date[i];
-//         // console.log(day);
-//         // day.toString();
-//     }
-// }
-
-// const date = new Date(Date.UTC(year, Number(month), Number(day)));
-// console.log(date.toLocaleDateString());
